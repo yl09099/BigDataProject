1d8e13f4e48210b728a63681f4c3bacfe16cf26b
yl09099 <shaq376260428@qq.com>
2018-12-27 15:05:59 +0800

    ....

--- Webproject/src/main/java/com/mini/service/FileDeal.java
+++ Webproject/src/main/java/com/mini/service/FileDeal.java
@@ -1,6 +1,7 @@
 package com.mini.service;
 
 
+import com.mini.dao.InsertDD;
 import com.mini.model.DingOragtion;
 
 import java.io.BufferedReader;
@@ -13,28 +14,28 @@ import java.io.InputStreamReader;
  */
 
 public class FileDeal {
+
     public static void main(String[] args) throws IOException {
         FileInputStream inputStream = new FileInputStream("E:/aa.txt");
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "gbk"));
         DingOragtion dingOragtion = new DingOragtion();
         String string = null;
         while ((string = bufferedReader.readLine()) != null) {
-            System.out.println(string);
-            String newString = string.replace('\"', ' ');
-            System.out.println("--------------------------------"+newString);
-            String[] str = string.split(":");
-            System.out.println(str.length);
+            String newString = string.replace('\"', ' ').replace(',', ':').replace('}',' ');
+            String[] str = newString.split(":");
+            for (String s : str) {
+                System.out.println(s);
+            }
             for (int i = 0; i < 1; i++) {
                 dingOragtion.setId(str[1].trim());
                 dingOragtion.setCreateDeptGroup(str[3].trim());
-             /*   dingOragtion.setName(str[5].trim());
+                dingOragtion.setName(str[5].trim());
                 dingOragtion.setParentid(str[7].trim());
-                dingOragtion.setAutoAddUser(str[9].trim());
-                dingOragtion.setSourceIdentifier(str[11]);*/
+                InsertDD.insert(dingOragtion);
             }
-            inputStream.close();
-            bufferedReader.close();
 
         }
+        inputStream.close();
+        bufferedReader.close();
     }
 }
--- Webproject/src/test/java/com/mini/DaoTest.java
+++ Webproject/src/test/java/com/mini/DaoTest.java
@@ -14,6 +14,7 @@ public class DaoTest {
 
     @Test
     public void test1() {
+
     }
 
 }
